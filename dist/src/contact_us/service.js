e5a6d6a014fb04746a79b99aa212cc8f158c40c5
unlimitedsucess <jiangbruce5@gmail.com>
2025-09-17 22:03:57 +0100

    initial commit

--- dist/src/contact_us/service.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.itemService = void 0;
-class ItemService {
-}
-exports.itemService = new ItemService();
--- dist/src/parcel/enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ParcelStatus = void 0;
-var ParcelStatus;
-(function (ParcelStatus) {
-    ParcelStatus["Pending"] = "pending";
-    ParcelStatus["Ongoing"] = "ongoing";
-    ParcelStatus["Delivered"] = "delivered";
-})(ParcelStatus || (exports.ParcelStatus = ParcelStatus = {}));
